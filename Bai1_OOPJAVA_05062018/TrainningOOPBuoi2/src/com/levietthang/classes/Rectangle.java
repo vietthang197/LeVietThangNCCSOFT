15f974be5afbaed38d7d8a04165853039df2ca78
Lê Việt Thắng <levietthang1997@gmail.com>
2018-06-06 16:58:44 +0700

    slss

---
+++ Bai1_OOPJAVA_05062018/TrainningOOPBuoi2/src/com/levietthang/classes/Rectangle.java
@@ -0,0 +1,29 @@
+package com.levietthang.classes;
+
+public class Rectangle extends Shape {
+
+    private float width, height;
+
+    public Rectangle(float pivotX, float pivotY, String color, float width, float height) {
+        super(pivotX, pivotY, color, KEY_RECTANGLE);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public float area() {
+        return width*height;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle : pivot( x:"+pivotX+", y: "+pivotY+"), color : "+color+", width : "+width+", height : "+height+" area :"+area();
+    }
+
+    public boolean isRectangle(){
+        if(width < height && width > 0 && height >0){
+            return false;
+        }
+        return true;
+    }
+}
